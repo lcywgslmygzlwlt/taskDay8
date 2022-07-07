3dcd225373d356e117555217c81441b21d02edf4
lcydr <1211753823@qq.com>
2022-07-07 19:10:50 +0800

    案例-tabbar

---
+++ src/components/MyTabBar.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="my-tab-bar">
+    <div
+      class="tab-item"
+      :class="{ current: item.componentName === currentInfo }"
+      v-for="item in list"
+      :key="item.componentName"
+      @click="clickFn(item.componentName)"
+    >
+      <!-- 图标 -->
+      <span class="iconfont" :class="item.iconText"></span>
+      <!-- 文字 -->
+      <span>{{ item.text }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentInfo: "MyGoodsList",
+    };
+  },
+  methods: {
+    clickFn(val) {
+      this.currentInfo = val;
+      this.$emit("changeInfo", val);
+    },
+  },
+  props: {
+    list: {
+      type: Array,
+      validator(val) {
+        console.log(val);
+        if (val.length >= 2 && val.length <= 5) {
+          return true;
+        }
+        return false;
+      },
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.my-tab-bar {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 50px;
+  border-top: 1px solid #ccc;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: white;
+  .tab-item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .current {
+    color: #1d7bff;
+  }
+}
+
+.current {
+  color: #1d7bff;
+}
+</style>
---
+++ src/views/MyGoodsList.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <MyTable :arr="list">
+      <template #header>
+        <th>#</th>
+        <th>商品名称</th>
+        <th>价格</th>
+        <th>标签</th>
+        <th>操作</th>
+      </template>
+      <!-- scope的值: {row: obj, index: 索引值} -->
+      <template #body="scope">
+        <td>{{ scope.row.id }}</td>
+        <td>{{ scope.row.goods_name }}</td>
+        <td>{{ scope.row.goods_price }}</td>
+        <td>
+          <input
+            type="text"
+            class="tag-input form-control"
+            style="width: 100px"
+            v-focus
+            v-model="scope.row.inputValue"
+            v-if="scope.row.inputShow"
+            @blur="scope.row.inputShow = false"
+            @keydown.enter="enterFn(scope.row)"
+          />
+          <button
+            class="btn btn-primary btn-sm add-tag"
+            v-else
+            @click="scope.row.inputShow = true"
+          >
+            +Tag
+          </button>
+          <span
+            style="margin-right: 8px"
+            class="badge badge-warning"
+            v-for="(str, ind) in scope.row.tags"
+            :key="ind"
+            >{{ str }}
+          </span>
+        </td>
+        <td>
+          <button class="btn btn-danger btn-sm" @click="btnDle(scope.row.id)">
+            删除
+          </button>
+        </td>
+      </template>
+    </MyTable>
+  </div>
+</template>
+
+<script>
+import MyTable from "../components/MyTable.vue";
+export default {
+  data() {
+    return {
+      list: [],
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    getList() {
+      this.$axios({
+        url: "/api/goods",
+      }).then((res) => {
+        res.data.data.forEach((ele) => {
+          ele.inputShow = false;
+        });
+        console.log(res);
+        this.list = res.data.data;
+      });
+    },
+    btnDle(id) {
+      const index = this.list.findIndex((ele) => ele.id == id);
+      this.list.splice(index, 1);
+    },
+    enterFn(obj) {
+      console.log(obj.inputValue, "===", obj.tags);
+      if (obj.inputValue.trim() == "") {
+        return alert("enter message");
+      }
+      obj.tags.push(obj.inputValue);
+      obj.inputValue = "";
+    },
+  },
+  components: {
+    MyTable,
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.my-goods-list {
+  .badge {
+    margin-right: 5px;
+  }
+}
+</style>
